d98abcc3886c15d68f9f340f5a52685b90497b6d
fourseagulls <cuatrogabiotas@gmail.com>
2025-11-30 00:22:04 +0100

    Se ha añadido un botón que abre una vista .fxml nueva como ventana independiente. Por ahora no están conectadas

--- src/main/java/org/maestra/MainApp.java
+++ src/main/java/org/maestra/MainApp.java
@@ -10,7 +10,7 @@ public class MainApp extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("MainView.fxml"));
-        Scene scene = new Scene(loader.load(), 400, 200);
+        Scene scene = new Scene(loader.load(), 600, 400);
         scene.getStylesheets().add(MainApp.class.getResource("style.css").toExternalForm());
         stage.setScene(scene);
         stage.setTitle("MaestraFX");
--- src/main/java/org/maestra/controllers/MainController.java
+++ src/main/java/org/maestra/controllers/MainController.java
@@ -1,7 +1,11 @@
 package org.maestra.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 public class MainController {
 
@@ -12,4 +16,27 @@ public class MainController {
     private void handleButtonAction() {
         label.setText("¡Botón pulsado!");
     }
+
+    @FXML
+    private void seguir() throws Exception {
+        // Load the FXML file
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/maestra/SiguienteView.fxml"));
+        Parent root = loader.load();
+
+        // Create a new stage
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root, 800, 600));
+
+        // Establece el tamaño mínimo de la ventana
+        stage.setMinWidth(800);
+        stage.setMinHeight(600);
+
+// Opcional: Establece el tamaño preferido
+        stage.setWidth(800);
+        stage.setHeight(600);
+
+        // Show the new stage
+        stage.show();
+    }
+
 }
